bda575904bd01fdfeda08a4b7700799e5cb5de81
zhuyuping <zhuyuping@meituan.com>
2018-04-04 19:53:12 +0800

    init 初始化项目提交

---
+++ asyncflow-flow/src/main/java/com/beautiful/asyncflow/App.java
@@ -0,0 +1,10 @@
+package com.beautiful.asyncflow;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
